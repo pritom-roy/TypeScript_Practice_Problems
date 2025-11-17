cb18dc539ff4267fee696a5ed06bc675f33fce49
pritom-roy <pritomroy20062@gmail.com>
2025-11-17 23:30:19 +0600

    solution update

---
+++ README.md
@@ -0,0 +1,67 @@
+keyof এর ব্যবহার ঃ
+
+যেকোন object এর key গুলকে union টাইপ হিসেবে ব্যবহার করতে হলে keyof ব্যবহার করতে হয় । 
+
+উদাহরণ - 
+type User = {
+  name: string;
+  age: number;
+  address: string;
+};
+
+type UserKey = keyof User; // "name" | "age" | "address"
+
+
+
+
+any, unknown , never এর বাবহার ঃ 
+
+any : যদি আমরা জানি নাহ কোন ধরনের টাইপ এখানে দরকার হবে অথবা যদি টাইপ কোন প্রভাব না ফেলে তাহলে আমরা any টাইপ হিসাবে ব্যবহার করতে পারি । এক কথায় টাইপ চেক করাই বন্ধ করে দেয়। 
+
+উদাহরণ - 
+function greeting(element: any): void {
+    console.log('hello')
+}
+
+unknown : any  এর মত যে কোন টাইপ রাখা যায় কিন্তু ব্যবহার করার আগে টাইপ চেক করে নিতে হয়। type-safe version of any
+
+উদাহরণ -
+value : unknown = "hello";
+
+if(value === 'string') console.log('hello');
+
+never : never এমন একটি type যা কখনো কোনো value return করে না। এটি সাধারণত error throw করার ক্ষেত্রে ব্যবহৃত হয়।
+
+উদাহরণ -
+const throwError = (msg: string): never => {
+    throw new Error(msg);
+};
+
+throwError('This is an error')
+
+Union and Intersection typer এর ব্যবহার ঃ 
+
+Union (|) : Union type এর মাধ্যমে একটি variable একাধিক type এর মধ্যে যেকোনো একটি হতে পারে।
+
+উদাহরণ -
+
+function greeting(element : string | number) : string{
+    return console.log(`hello ${element}`);
+}
+এই ফাংশন এ string অথবা number ব্যবহার করা যাবে ।
+
+Intersection (&) : Intersection type একাধিক type কে একসাথে combine করে। 
+
+উদাহরণ -
+type User = {
+  name: string;
+  age: number;
+};
+
+type role = {
+  position : string;
+};
+
+type UserWithRole = User & role;
+
+
---
+++ solution.ts
@@ -0,0 +1,76 @@
+function formatValue(element: number | string | boolean): number | string | boolean {
+    if (typeof (element) === 'string') {
+        return element.toUpperCase();
+    }
+    else if (typeof (element) === 'number') {
+        return element * 10;
+    }
+    else if (typeof (element) === 'boolean') {
+        return !element;
+    }
+    else return 0;
+};
+
+function getLength(element: string | object): number {
+    if (typeof (element) === 'string') {
+        return element.length;
+    }
+    else if (Array.isArray(element)) {
+        return element.length;
+    }
+    else return 0;
+}
+
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    getDetails() {
+        return `Name: [${this.name}], Age: [${this.age}]`;
+    }
+}
+
+interface elementType {
+    title: string;
+    rating: number;
+}
+
+function filterByRating(elements: elementType[]): elementType[] {
+    return elements.filter(i => i.rating >= 4)
+}
+
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+function printBookDetails(element: Book): void {
+    console.log(`Title: [${element.title}], Author: [${element.author}], Published: [${element.publishedYear}], Available: [${element.isAvailable ? 'Yes' : 'No'}]`);
+}
+
+
+function getUniqueValues<T>(arr1: T[], arr2: T[]): T[] {
+    return Array.from(new Set([...arr1, ...arr2]));
+}
+
+interface Product {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+}
+
+function calculateTotalPrice(element: Product[]): number {
+    if (element.length === 0) return 0;
+    const ans = element.reduce((total, i) => {
+        const sum = i.price * i.quantity;
+        const discount = (i.discount === undefined) ? 0 : (sum * i.discount) / 100;
+        return total + sum - discount;
+    }, 0)
+
+    return ans;
+}
